f26b291f1aa9e3c46b8cc23bdb688cc22b995e62
jose carlo san pedro <josecarlosanpedro0302@gmail.com>
2020-02-06 00:02:27 +0800

    upload

--- src/modules/Home/View.jsx
+++ src/modules/Home/View.jsx
@@ -1,13 +1,56 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Spin from 'antd/lib/spin';
 import isEmpty from 'lodash/isEmpty'
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
 
 const Home = props => {
-
+  
+  const [uploading, setUploading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [fileName, setFileName] = useState("");
+  const [url, setURL] = useState("");
+  const handleUploadStart = () => {
+    setUploading(true)
+    setProgress(0)
+  }  
+  const handleProgress = progress => {
+    setProgress(progress)
+  }
+  const handleUploadError = error => {
+    setUploading(false)
+    console.error(error);
+  };
+  const handleUploadSuccess = name => {
+    setFileName(name)
+    setProgress(100)
+    setUploading(false)
+    firebase
+      .storage()
+      .ref("file")
+      .child(name)
+      .getDownloadURL()
+      .then(url => {
+        setURL(url)
+        console.log(url, 'url')
+      });
+      
+  };
   return (
     <section className="home-section">
       Home
+      {url}
+      <FileUploader
+            accept="*"
+            name="file"
+            randomizeFilename
+            storageRef={firebase.storage().ref("file")}
+            onUploadStart={handleUploadStart}
+            onUploadError={handleUploadError}
+            onUploadSuccess={handleUploadSuccess}
+            onProgress={handleProgress}
+          />
     </section>
   )
 }
